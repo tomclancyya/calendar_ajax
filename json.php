097e94648fd6cbd9d7cd1b450cb0fb49320cc9f5
Yan Lisitsin <tomclancyya@gmail.com>
2012-09-23 04:12:18 +0900

    added tablesorting, deleted trash

--- json.php
+++ json.php
@@ -8,7 +8,23 @@ $array = array(
     "1348223000" => "event 2",
     "1348223001" => "event 3",
     "1348223002" => "event 4",
-    "1348239800" => "event 5",
+    "1348239800" =>  array(
+         "dimensional" => "jjj",
+		 "array" => "foo"
+         )
+
+);
+
+$array2 = array(
+   array( "1348000628", "event 0", "ddd"),
+    "1348220628" => "event 1",
+    "1348223000" => "event 2",
+    "1348223001" => "event 3",
+    "1348223002" => "event 4",
+    "1348239800" =>  array(
+         "dimensional" => "jjj",
+		 "array" => "foo"
+         )
 
 );
  
@@ -21,7 +37,10 @@ $array = array(
 $newArray = array();
 foreach ($array as $k => $v) {
     if (($k > $beginTime) AND ($k < $endTime)){
-      array_push($newArray,$v);   
+      //array_push($newArray,$v); 
+		$subarray = array($k,$v,"pizda");
+		//array_push($newArray,$subarray);
+	   $newArray[] = $subarray;
      }
     	
 }
